1b59febabd92dbd9003d70a3c6a9091d19465c01
LostAsleep <phammersen@protonmail.com>
2021-08-19 21:39:42 +0200

    Minimal serving.

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"html"
 	"log"
 	"net/http"
 )
 
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	log.Println("Ouch.") // Print w/ timestampt to stdout
+	//Print to the browser (on served site)
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+
+	//println("Someone just accessed the server.")
+	//fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
 func main() {
-	// After this there is literally a server running!
+	http.HandleFunc("/", sayHello)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
