7cc56b67ab5e1ba7152479b3606a69f707d604db
LostAsleep <phammersen@protonmail.com>
2021-08-20 22:36:30 +0200

    Rename func and add more form data to return.

--- main.go
+++ main.go
@@ -6,23 +6,18 @@ import (
 	"net/http"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	rmet := fmt.Sprintf("RequestMethod: %v\n", r.Method)
-	ruri := fmt.Sprintf("RequestURI: %v\n", r.RequestURI)
-
-	// Print w/ timestampt to stdout
-	log.Printf(ruri)
-	log.Printf(rmet)
-
-	//Print to the browser (on served site)
-	// fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	fmt.Fprintf(w, ruri)
-
-	//println("Someone just accessed the server.")
-	//fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+func returnForm(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // PostForm is only available after ParseForm is called.
+	ruri := fmt.Sprintf(`
+	RequestURI: %v
+	Host:       %v
+	Values:     %v
+	`, r.RequestURI, r.Host, r.Form)
+	//log.Printf(ruri)  // Print w/ timestampt to stdout
+	fmt.Fprintf(w, ruri) // Print to the browser (on served site)
 }
 
 func main() {
-	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/", returnForm)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
