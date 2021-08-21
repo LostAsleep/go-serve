eb02bdd3b7dcb4eb174d5a9431b2091c49ef4a9b
LostAsleep <phammersen@protonmail.com>
2021-08-21 09:28:17 +0200

    Clean up, add another http.HandleFunc.

--- main.go
+++ main.go
@@ -6,18 +6,30 @@ import (
 	"net/http"
 )
 
-func returnForm(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() // PostForm is only available after ParseForm is called.
-	ruri := fmt.Sprintf(`
-	RequestURI: %v
-	Host:       %v
-	Values:     %v
-	`, r.RequestURI, r.Host, r.Form)
-	//log.Printf(ruri)  // Print w/ timestampt to stdout
-	fmt.Fprintf(w, ruri) // Print to the browser (on served site)
+const tpl = `
+RequestURI: %v
+Host:       %v
+Form:       %v
+    some:   %v
+`
+
+func reflectForm(w http.ResponseWriter, r *http.Request) {
+	// PostForm is only available after ParseForm is called.
+	r.ParseForm()
+	fmt.Fprintf(w, fmt.Sprintf(tpl, // Print to browser
+		r.RequestURI,
+		r.Host,
+		r.Form,
+		r.Form.Get("some"),
+	))
+}
+
+func some(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "something")
 }
 
 func main() {
-	http.HandleFunc("/", returnForm)
+	http.HandleFunc("/", reflectForm)
+	http.HandleFunc("/some", some)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
